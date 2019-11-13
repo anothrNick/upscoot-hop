865c7528bc35d3a2f018f063a16b73f38d3c8dd5
anothrNick <nick.sjostrom12@gmail.com>
2019-11-13 16:07:54 -0500

    Updates

---
+++ precache-manifest.06c692137073f1c43b7e1f7a25c85c03.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b89bcdabc9a2c27ab9b4ed6b615de2e4",
+    "url": "/upscoot-hop/index.html"
+  },
+  {
+    "revision": "4261f5f16753e6b65e51",
+    "url": "/upscoot-hop/static/css/2.0f87f62a.chunk.css"
+  },
+  {
+    "revision": "f46e480adee637c6570d",
+    "url": "/upscoot-hop/static/css/main.76621772.chunk.css"
+  },
+  {
+    "revision": "4261f5f16753e6b65e51",
+    "url": "/upscoot-hop/static/js/2.43ac66fd.chunk.js"
+  },
+  {
+    "revision": "f46e480adee637c6570d",
+    "url": "/upscoot-hop/static/js/main.cea7b64f.chunk.js"
+  },
+  {
+    "revision": "1390e846f503659cf276",
+    "url": "/upscoot-hop/static/js/runtime-main.7b36f59d.js"
+  }
+]);
